e32be89afeebcf75315b1c1acd9cc487c9b733ab
albaadmin <abujari@gmail.com>
2019-12-13 13:52:23 +0100

    init.

---
+++ curl-dependency-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TOKEN=$(curl -X POST -H 'Accept: application/json' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Authorization: Basic b3BlbmJhdG9uT1NDbGllbnQ6c2VjcmV0'  --data 'username=admin&password=openbaton&grant_type=password' http://localhost:8080/oauth/token | jq -r '.value')
+
+curl -X POST -H 'Accept: application/json' -H 'project-id: 5c1ad332-d082-46b6-b3d7-94497c7468ad' -H "Authorization: Bearer ${TOKEN}" http://localhost:8080/api/v1/wizzard/dependency/e8f23360-9327-490e-9c4b-b288a3f93009/vnfName/iperf-server/ipAddr/127.0.0.1
+
+
